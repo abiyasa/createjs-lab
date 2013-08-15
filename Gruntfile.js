e4a1d78e5b34e691b64218caa675387dd35e5756
abiyasa <ilham.suhardi@gmail.com>
2013-08-15 16:25:05 +0200

    Add jshint to Grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,33 @@
 module.exports = function(grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        connect: {
-            server: {
-                options: {
-                    port: 9001,
-                    base: '.',
-                    keepalive: true
-                }
-            }
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          base: '.',
+          keepalive: true
         }
-    });
+      }
+    },
 
-    grunt.loadNpmTasks('grunt-contrib-connect');
+    jshint: {
+      files: ['js/*.js'],
+      options: {
+        browser: true,
+        globals: {
+          createjs: true
+        }
+      }
+    }    
+  });
 
-    // Default task(s).
-    grunt.registerTask('default', ['connect']);
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task(s).
+  grunt.registerTask('default', ['connect']);
 };
