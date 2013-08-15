629650dbfa623f0d6726c50430246fa9d74d2c39
abiyasa <ilham.suhardi@gmail.com>
2013-08-15 16:30:53 +0200

    Add simple hello world

---
+++ js/00-helloWorld.js
@@ -0,0 +1,48 @@
+var myLab = myLab || {};
+
+(function (ns) {
+
+  // the main stage
+  var stage;
+
+  // Init the stage
+  initStage = function (canvasId) {
+    var canvas = document.getElementById(canvasId);
+    if (canvas) {
+      stage = new createjs.Stage(canvas);
+    }
+  };
+
+  // init  the application
+  ns.init = function () {
+    initStage('canvas-main');
+
+    // create container box    
+    var containerBox = new createjs.Container();
+
+    // add rectangular shape
+    var bgShape = new createjs.Shape();
+    var g = bgShape.graphics;
+    g.beginFill('#009eef');
+    g.drawRect(0, 0, 100, 100);
+    g.endFill();
+    containerBox.addChild(bgShape);
+
+    // add text into container
+    var theText = new createjs.Text('Hello World!', 'bold 16px Metal,Arial', '#FFFFFF');
+    theText.x = 10;
+    theText.y = 30;
+    theText.lineWidth = 80;
+    containerBox.addChild(theText);
+
+    // add to main stage
+    stage.addChild(containerBox);
+    containerBox.x = 50;
+    containerBox.y = 20;
+
+    // draw stage
+    stage.update();
+  };
+
+
+})(myLab);
